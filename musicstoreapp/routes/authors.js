b8467447c9a015d65bf2537896c705afc8dfa465
Stelian Adrian Stanci <UO277653@uniovi.es>
2022-03-29 12:37:18 +0200

    SDI–2122-212-sesión7-Ejercicio-complementario2

--- musicstoreapp/routes/authors.js
+++ musicstoreapp/routes/authors.js
@@ -32,7 +32,15 @@ module.exports = function (app) {
     });
 
     app.get('/authors/add', function (req, res) {
-        res.render("authors/add.twig");
+
+        let roles = ["Cantante","Batería","Guitarrista","Bajista","Teclista"]
+
+        let response = {
+            seller:'Autores',
+            roles:roles
+        }
+
+        res.render("authors/add.twig", response);
     });
 
     app.get('/authors/*', function (req, res) {
