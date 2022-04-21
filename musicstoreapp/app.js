0d244793ba0a7743d87df64e330386c672855f99
Stelian Adrian Stanci <UO277653@uniovi.es>
2022-04-21 17:56:08 +0200

    SDI-2122-212-10.3-SW Cliente JQuery + AJAX

--- musicstoreapp/app.js
+++ musicstoreapp/app.js
@@ -5,6 +5,15 @@ let cookieParser = require('cookie-parser');
 let logger = require('morgan');
 let app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "POST, GET, DELETE, UPDATE, PUT");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, token");
+  // Debemos especificar todas las headers que se aceptan. Content-Type , token
+  next();
+});
+
 let bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
